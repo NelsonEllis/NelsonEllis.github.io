79e94e2f2ba625715ff8b23d336888559f477f29
NezeGamer <nelson_905@hotmail.com>
2020-04-24 16:32:56 -0400

    Se añadio la interaccion de la barra de navegacion y la barra de busqueda, se empezo a desarrollar la vista de las imagenes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Conectandonos con la libreria de Muuri
 const grid = new Muuri(".grid", {
     layout: {
         rounding: false
@@ -7,4 +8,24 @@ const grid = new Muuri(".grid", {
 window.addEventListener('load', () => {
     grid.refreshItems().layout();
     document.getElementById('grid').classList.add('imagenes-cargadas');
+
+    // Agregamos los listener de los enlaces para filtar por categoria.
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach((elemento) => {
+        elemento.addEventListener('click', (evento) => {
+            evento.preventDefault();
+            enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+            evento.target.classList.add('activo');
+
+            const categoria = evento.target.innerHTML.toLowerCase();
+            categoria === 'all' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+        });
+    });
+
+    // Agregamos el listener para la barra de busqueda.
+    document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+        const busqueda = evento.target.value;
+        grid.filter((item) => item.getElement().dataset.etiquetas.includes(busqueda));
+    });
 });
+
