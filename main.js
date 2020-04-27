bc354f407640124f08a45359564daa0679408c4a
NezeGamer <nelson_905@hotmail.com>
2020-04-27 18:16:27 -0400

    Se termino la interaccion de las imagenes y el footer

--- main.js
+++ main.js
@@ -31,14 +31,25 @@ window.addEventListener('load', () => {
     // Agregamos listener para las imagenes
     const overlay = document.getElementById('overlay');
     document.querySelectorAll('.grid .item img').forEach((elemento) => {
+        elemento.addEventListener('click', () => {
         const ruta = elemento.getAttribute('src');
         const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
 
-        elemento.addEventListener('click', () => {
+        
         overlay.classList.add('activo');
         document.querySelector('#overlay img').src = ruta;
         document.querySelector('#overlay .descripcion').innerHTML = descripcion;
         });
+    });
+
+    // Eventlistener del boton de cerrar
+    document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+        overlay.classList.remove('activo');
+    });
+
+    // Eventlistener del overlay
+    overlay.addEventListener('click', (evento) => {
+        evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
     });
 });
 
